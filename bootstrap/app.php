6935bcea6f21cfa6cbe14bd173f8660eb0d96832
José Daniel Mendes da Palma Guerreiro <joseguerreiro@pge.sc.gov.br>
2022-01-05 19:14:32 -0300

    ajustes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,9 @@
 <?php
 
 /*
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
+/*
 |--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
